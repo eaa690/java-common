3f44a7b80bab018482accd1d5d217744588255bf
Brian Michael <bsmichae42@yahoo.com>
2022-02-27 09:17:04 -0500

    Initial checkin

---
+++ src/main/java/org/eaa690/javacommon/JavaCommonApplication.java
@@ -0,0 +1,13 @@
+package org.eaa690.javacommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCommonApplication.class, args);
+	}
+
+}
